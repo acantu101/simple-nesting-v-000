429a8caaf9514830f56ca78a47545e08e0d153b3
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-28 19:48:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     programmer_hash[:alan_kay][:known_for]= "GUI"
+programmer_hash[:alan_kay][:known_for]= "GUI"
 programmer_hash
 
 end
@@ -126,5 +126,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
